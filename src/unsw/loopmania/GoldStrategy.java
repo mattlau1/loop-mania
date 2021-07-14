5bb842826c0514bcae1b32427795bd07cc9d34e2
Raymond Chung <raymond.chung216@gmail.com>
2021-07-15 00:35:37 +1000

    Added some strategies for gold and health potion items(atk/def multipliers etc), usage still needs to be done

---
+++ src/unsw/loopmania/GoldStrategy.java
@@ -0,0 +1,44 @@
+package unsw.loopmania;
+
+public class GoldStrategy implements ItemStrategy{
+    @Override
+    /**
+     *  Gold item does not have any attack mechanics, so returns 1
+     * @param enemy The monster/entity that the Character is in combat with
+     * @return The attack multiplier against the monster, may vary depending on
+     *          the monster type
+     */
+    public double atkMuliplier(BasicEnemy enemy) {
+        return 1;
+    }
+
+    @Override
+    /**
+     *  Gold item does not have any defence mechanics, so returns 1
+     * @param enemy The monster/entity that the Character is in combat with
+     * @return The defence multiplier against the monster, can vary depending on
+     *          the monster type
+     */
+    public double defMuliplier(BasicEnemy enemy) {
+        return 1;
+    }
+
+    @Override
+    /**
+     *  Gold item does not have any critical hit mechanics, so returns 1
+     * @param enemy The monster/entity that the Character is in combat with
+     * @return The crit multiplier against the monster, can vary depending on
+     *          the monster type
+     */
+    public double critMuliplier(BasicEnemy enemy) {
+        return 1;
+    }
+
+    @Override
+    /**
+     *  Gold item does not have any on hit effects so does nothing
+     * @param enemy The monster/entity that the Character is in combat with
+     */
+    public void onHitEffects(BasicEnemy enemy) {}
+
+}
