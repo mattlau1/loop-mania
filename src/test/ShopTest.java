f22ccba9d9c0ea266327d4918d8016dae3553c7f
Raymond Chung <raymond.chung216@gmail.com>
2021-07-19 08:34:09 +1000

    Added tests for buying items

---
+++ src/test/ShopTest.java
@@ -0,0 +1,53 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.javatuples.Pair;
+import org.junit.jupiter.api.Test;
+
+import unsw.loopmania.Character;
+import unsw.loopmania.LoopManiaWorld;
+import unsw.loopmania.LoopManiaWorldControllerLoader;
+import unsw.loopmania.PathPosition;
+import unsw.loopmania.Enemies.Enemy;
+import unsw.loopmania.Enemies.SlugEnemy;
+import unsw.loopmania.Enemies.VampireEnemy;
+import unsw.loopmania.Enemies.ZombieEnemy;
+import unsw.loopmania.Items.Item;
+import unsw.loopmania.Items.SwordStrategy;
+
+public class ShopTest {
+  @Test
+  public void testBuyItem() {
+      // tests if buying an item deducts money and adds it to inventory
+      TestSetup s = new TestSetup();
+      LoopManiaWorld d = s.makeTestWorld();
+      Character testChar = new Character(new PathPosition(1, d.getOrderedPath()));
+      d.setCharacter(testChar);
+      testChar.addGold(1000);
+      d.buyItem(new SwordStrategy());
+      List<Item> unequipedItems = d.getUnequip();
+      assertTrue(unequipedItems.get(0).getStrategy() instanceof SwordStrategy);
+      assertEquals(900, testChar.getGold());
+  }
+
+  @Test
+  public void testBuyItemNoGold() {
+      // tests if buying an item deducts money and adds it to inventory
+      TestSetup s = new TestSetup();
+      LoopManiaWorld d = s.makeTestWorld();
+      Character testChar = new Character(new PathPosition(1, d.getOrderedPath()));
+      d.setCharacter(testChar);
+      testChar.addGold(50);
+      d.buyItem(new SwordStrategy());
+      List<Item> unequipedItems = d.getUnequip();
+      assertTrue(unequipedItems.size() == 0);
+      assertEquals(50, testChar.getGold());
+  }
+
+}
