09ca69fdf73cd8fc227f3b9b9e3e9f0ddedd21d7
Michael <michael.chidiac2@gmail.com>
2021-07-19 06:34:35 +1000

    finished tests for goals

---
+++ src/test/GoalTest.java
@@ -0,0 +1,79 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import unsw.loopmania.Character;
+import unsw.loopmania.LoopManiaWorld;
+import unsw.loopmania.PathPosition;
+import unsw.loopmania.Goals.CycleGoal;
+import unsw.loopmania.Goals.ExperienceGoal;
+import unsw.loopmania.Goals.Goal;
+import unsw.loopmania.Goals.GoldGoal;
+
+public class GoalTest {
+
+    @Test
+    public void testGoldGoal() {
+        TestSetupNoGoals s = new TestSetupNoGoals();
+        Goal goal = new Goal();
+        goal.addGoal(new GoldGoal(200));
+        LoopManiaWorld d = s.makeTestWorld(goal);
+        Character testChar = new Character(new PathPosition(1, d.getOrderedPath()));
+        d.setCharacter(testChar);
+        assertEquals(false, goal.isGameWon(testChar));
+        testChar.addGold(200);
+        assertEquals(true, goal.isGameWon(testChar));
+    }
+
+    @Test
+    public void testExpGoal() {
+        TestSetupNoGoals s = new TestSetupNoGoals();
+        Goal goal = new Goal();
+        goal.addGoal(new ExperienceGoal(200));
+        LoopManiaWorld d = s.makeTestWorld(goal);
+        Character testChar = new Character(new PathPosition(1, d.getOrderedPath()));
+        d.setCharacter(testChar);
+        assertEquals(false, goal.isGameWon(testChar));
+        testChar.addEXP(200);
+        assertEquals(true, goal.isGameWon(testChar));
+    }
+
+    @Test
+    public void testCycleGoal() {
+        TestSetupNoGoals s = new TestSetupNoGoals();
+        Goal goal = new Goal();
+        goal.addGoal(new CycleGoal(20));
+        LoopManiaWorld d = s.makeTestWorld(goal);
+        Character testChar = new Character(new PathPosition(1, d.getOrderedPath()));
+        d.setCharacter(testChar);
+        assertEquals(false, goal.isGameWon(testChar));
+        while (testChar.getCycleCount() < 20) {
+            testChar.incrementCycleCount();
+        }
+        assertEquals(true, goal.isGameWon(testChar));
+    }
+
+    @Test
+    public void testComplexGoal() {
+        TestSetupNoGoals s = new TestSetupNoGoals();
+        Goal goal = new Goal();
+        goal.addGoal(new GoldGoal(200));
+        goal.addGoal(new CycleGoal(20));
+        goal.addGoal(new ExperienceGoal(200));
+        LoopManiaWorld d = s.makeTestWorld(goal);
+        Character testChar = new Character(new PathPosition(1, d.getOrderedPath()));
+        d.setCharacter(testChar);
+        assertEquals(false, goal.isGameWon(testChar));
+        testChar.addGold(200);
+        assertEquals(false, goal.isGameWon(testChar));
+        while (testChar.getCycleCount() < 20) {
+            testChar.incrementCycleCount();
+        }
+        assertEquals(false, goal.isGameWon(testChar));
+        testChar.addEXP(200);
+        assertEquals(true, goal.isGameWon(testChar));
+    }
+}
