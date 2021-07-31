1ee001403e3222177aa35aa9abef77d67f8d3c0a
Michael <michael.chidiac2@gmail.com>
2021-08-01 02:26:28 +1000

    created test for load card and card to building

--- src/test/WorldTest.java
+++ src/test/WorldTest.java
@@ -1,12 +1,14 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.css.Size;
 import unsw.loopmania.Character;
 import unsw.loopmania.LoopManiaWorld;
 import unsw.loopmania.PathPosition;
@@ -19,21 +21,39 @@ public class WorldTest {
 
     @Test
     public void testCardToBuilding() {
-        // setup world
+        // check that card to building generates a building and destroys the card
+        TestSetup setup = new TestSetup();
+        LoopManiaWorld world = setup.makeTestWorld();
+        // create card
+        world.generateCardDrops();
+        Card testCard = world.loadCard();
+        // convert card to building
+        Building testBuilding = world.convertCardToBuildingByCoordinates(testCard.getX(), testCard.getY(), 1, 1);
+        if (testBuilding == null) {
+            // building type was not able to be placed on tile
+            testBuilding = world.convertCardToBuildingByCoordinates(testCard.getX(), testCard.getY(), 2, 2);
+        }
+        assertNotEquals(null, testBuilding);
+        // get list of buildings after card is used
+        List<Building> buildingList = world.getBuildings();
+        // assert second building list is greater than the first by 1
+        assertEquals(1, buildingList.size());
+        // assert there are no cards left
+        assertEquals(0, world.getCards().size());
+
+    }
+
+    @Test
+    public void testLoadCard() {
+        // Test that load card creates a card in the world
         TestSetup setup = new TestSetup();
         LoopManiaWorld world = setup.makeTestWorld();
         // get list of buildings before card is used
-        List<Building> buildingList = world.getBuildings();
         // create card
-        VillageCardStrategy villageCardStrategy = new VillageCardStrategy();
-
-        Card testCard = new Card(new SimpleIntegerProperty(0), new SimpleIntegerProperty(0), villageCardStrategy);
-        // convert card to building
-        world.convertCardToBuildingByCoordinates(testCard.getX(), testCard.getY(), 1, 1);
-        // get list of buildings after card is used
-        List<Building> buildingListAfter = world.getBuildings();
-        // assert second building list is greater than the first by 1
-        assertEquals((buildingList.size() + 1), buildingListAfter.size());
+        world.generateCardDrops();
+        world.loadCard();
+        List<Card> cardList = world.getCards();
+        assertEquals(cardList.size(), 1);
 
     }
 
