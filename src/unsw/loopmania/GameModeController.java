de1f69b9275446fc2be0e4498a0cce4c04b24705
Matthew Lau <matthew.lau78@gmail.com>
2021-08-02 06:54:25 +1000

    gamemode sound fx

--- src/unsw/loopmania/GameModeController.java
+++ src/unsw/loopmania/GameModeController.java
@@ -1,11 +1,14 @@
 package unsw.loopmania;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /**
  * controller for the main menu. TODO = you could extend this, for example with
@@ -17,9 +20,23 @@ public class GameModeController {
   private MenuSwitcher mapSelectionSurvivalSwitcher;
   private MenuSwitcher mapSelectionBerserkerSwitcher;
   private MenuSwitcher mapSelectionConfusingSwitcher;
+  MediaPlayer buttonClick;
+  MediaPlayer buttonHover;
 
   @FXML
   Button backButton;
+
+  @FXML
+  Button standardModeButton;
+
+  @FXML
+  Button berserkerModeButton;
+
+  @FXML
+  Button survivalModeButton;
+
+  @FXML
+  Button confusingModeButton;
 
   /**
    * called when controller is initialised, sets the button hover effects
@@ -27,6 +44,77 @@ public class GameModeController {
   @FXML
   private void initialize() {
     setButtonHoverEffects();
+  }
+
+  /**
+   * Sets the button hover effects
+   */
+  private void setButtonHoverEffects() {
+    standardModeButton.setStyle("-fx-background-color:transparent;");
+
+    standardModeButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        buttonHoverSound();
+        standardModeButton.setStyle("-fx-background-color:#cccccc; -fx-text-fill: #333333");
+      }
+    });
+
+    standardModeButton.setOnMouseExited(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        standardModeButton.setStyle("-fx-background-color:transparent; -fx-text-fill: white");
+      }
+    });
+
+    berserkerModeButton.setStyle("-fx-background-color:transparent;");
+
+    berserkerModeButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        buttonHoverSound();
+        berserkerModeButton.setStyle("-fx-background-color:#cccccc; -fx-text-fill: #333333");
+      }
+    });
+
+    berserkerModeButton.setOnMouseExited(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        berserkerModeButton.setStyle("-fx-background-color:transparent; -fx-text-fill: white");
+      }
+    });
+    survivalModeButton.setStyle("-fx-background-color:transparent;");
+
+    survivalModeButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        buttonHoverSound();
+        survivalModeButton.setStyle("-fx-background-color:#cccccc; -fx-text-fill: #333333");
+      }
+    });
+
+    survivalModeButton.setOnMouseExited(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        survivalModeButton.setStyle("-fx-background-color:transparent; -fx-text-fill: white");
+      }
+    });
+    confusingModeButton.setStyle("-fx-background-color:transparent;");
+
+    confusingModeButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        buttonHoverSound();
+        confusingModeButton.setStyle("-fx-background-color:#cccccc; -fx-text-fill: #333333");
+      }
+    });
+
+    confusingModeButton.setOnMouseExited(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent t) {
+        confusingModeButton.setStyle("-fx-background-color:transparent; -fx-text-fill: white");
+      }
+    });
   }
 
   /**
@@ -115,24 +203,23 @@ public class GameModeController {
   }
 
   /**
-   * Set the button hover effects
+   * sound effect for the button clicking
    */
-  private void setButtonHoverEffects() {
-    backButton.setStyle("-fx-background-color:transparent;");
+  public void buttonClickSound() {
+    String path = "src/audio/buttonClick.wav";
+    Media music = new Media(Paths.get(path).toUri().toString());
+    buttonClick = new MediaPlayer(music);
+    buttonClick.play();
+  }
 
-    backButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent t) {
-        backButton.setStyle("-fx-background-color:#cccccc; -fx-text-fill: #333333");
-      }
-    });
-
-    backButton.setOnMouseExited(new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent t) {
-        backButton.setStyle("-fx-background-color:transparent; -fx-text-fill: white");
-      }
-    });
+  /**
+   * sound effect for the button hovering
+   */
+  public void buttonHoverSound() {
+    String path = "src/audio/buttonHover.wav";
+    Media music = new Media(Paths.get(path).toUri().toString());
+    buttonHover = new MediaPlayer(music);
+    buttonHover.play();
   }
 
 }
